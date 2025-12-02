3e1c5a3e7f916427d57e1e33f85175056d3421af
지민승 <84808237+alstmd5310@users.noreply.github.com>
2025-12-03 02:28:24 +0900

    사운드조절기능, 모든맵연결, 뒤로가기

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // UI 기능을 쓰기 위해 필수!
+
+public class SoundManager : MonoBehaviour
+{
+    public GameObject soundPopup; // 소리 조절 창 (패널)
+
+    // 1. 소리 버튼 누르면 창 켜기
+    public void OpenPopup()
+    {
+        soundPopup.SetActive(true);
+    }
+
+    // 2. 닫기 버튼 누르면 창 끄기
+    public void ClosePopup()
+    {
+        soundPopup.SetActive(false);
+    }
+
+    // 3. 슬라이더를 움직일 때마다 실행되는 함수
+    // volume에는 0.0 ~ 1.0 사이의 값이 들어옵니다.
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume; // 게임 전체 소리 크기 조절
+        Debug.Log("현재 볼륨: " + volume);
+    }
+}
---
+++ Assets/SoundOpenBtn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SoundOpenBtn : MonoBehaviour
+{
+    // 여기에 하이어라키에 있는 SoundManager를 끌어다 놓을 겁니다.
+    public SoundManager soundManager;
+
+    // 마우스로 이 그림을 클릭하면 실행됨
+    private void OnMouseDown()
+    {
+        // SoundManager야, 팝업창 좀 열어줘!
+        if (soundManager != null)
+        {
+            soundManager.OpenPopup();
+        }
+    }
+}
